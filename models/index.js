74f0c377c54aad94ca8ea8068459efa6c864767f
danialmirza99 <danialmirza99@gmail.com>
2022-11-04 20:54:40 -0700

    finished index model

--- models/index.js
+++ models/index.js
@@ -3,15 +3,35 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { getEnabledCategories } = require('trace_events');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_tags'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_products'
+});
 module.exports = {
   Product,
   Category,
